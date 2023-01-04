bd679438f0ce13655c07ebbac8d4a95514b30694
Moustafa Loukarfi <88500328+SomeBoDy1105@users.noreply.github.com>
2023-01-04 13:45:17 +0000

    pointers update

---
+++ Tutos/Pointers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char character = 'M';
+    char * pointer_to_Character = &character;
+    int integer = 2;
+    int * pointer_to_integer = &integer;
+	float floater = 3.14;
+    float * pointer_to_floater = &floater;
+    //display the value of each pointer
+    printf("the value pointer_to_Character is : %c\n",*pointer_to_Character);
+    printf ("the value pointer_to_integer is : %d\n",*pointer_to_integer);
+    printf("the value pointer_to_floater is : %f\n", *pointer_to_floater);
+    // note : "\n" is used to go back in line so the display won't be messed up
+    return 0;
+}
