ef3fd5d7d3f39639e20db45cc7bfb3b61692fddc
Moustafa Loukarfi <88500328+SomeBoDy1105@users.noreply.github.com>
2023-01-08 22:12:32 +0000

    stuff

--- Univ/Create.c
+++ Univ/Create.c
@@ -3,30 +3,33 @@
 #include <stdbool.h>
 
 typedef struct element element;
-typedef element *list;
+typedef element *liste;
 struct element
 {
-    int info;
-    list svt;
+    int val;
+    liste suivt;
 };
-void create(list tete, int n)
+liste tete = NULL;
+void creer_liste (liste *tete ,int n)
 {
-    list nouv, ptr;
-    nouv = (list)malloc(sizeof(list));
-    printf("\nEnter the data value for the node:\t");
-    scanf("%d", &nouv->info);
-    nouv->svt = NULL;
-    if (tete == NULL)
-    {
-        tete = nouv;
-    }
-    else
-    {
-        ptr = tete;
-        while (ptr->svt != NULL)
-        {
-            ptr = ptr->svt;
-        }
-        ptr->svt = nouv;
-    }
-}
+	int i,x;
+	liste nouv;
+	for(i = 0; i < n; i++)
+	{
+		printf("Enter value %d: ", i+1);
+        scanf("%d", &x);
+		/*start of function*/
+		nouv = (liste)malloc(sizeof(element));
+		nouv->val = n;
+		nouv->suivt = NULL;
+		if (tete == NULL) {
+			tete = nouv;
+			return;
+		}
+		liste temp = tete;
+		while (temp->suivt != NULL){
+			temp = temp->suivt;
+			temp->suivt = nouv;
+		}
+	}
+		
